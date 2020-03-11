6447763e7d5c2ea9c810ab44175252e958695604
Manuel Jürgen Schuler <manuel.schuler@student.uibk.ac.at>
2020-03-11 16:08:53 +0100

    Cleanup and add some additional functionality
        ~ rename all aliases to <file>.alias
        ~ rename all functions to <file>.func
        + Add new function pdiff.func outputting colorized diffs
          inside a pager (less) if it doesn't fit to screen
        + Add screenrc
        ~ Update inputrc and install.sh
        ~ Simplify sourcing section of bashrc

--- install.sh
+++ install.sh
@@ -5,13 +5,14 @@ list=(
     bashrc
     inputrc
     profile
+    screenrc
+    tmux.conf
     toprc
     xinitrc
 )
 
-# set appropriate paths for alias and function directory
-sed -i "s+\(alias_dir=\).*$+\1${PWD}/bash_aliases+"   bashrc
-sed -i  "s+\(func_dir=\).*$+\1${PWD}/bash_functions+" bashrc
+# set path to dotfiles in bashrc
+sed -i "s+\(dotfiles_dir=\).*$+\1${PWD}+" bashrc
 
 # link all files in list to ${HOME}/.<entry>
 for i in ${list[@]} ; do
